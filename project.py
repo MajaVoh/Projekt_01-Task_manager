26042d3b47cab2b35dd0f7c612adb6a4c3b176db
marie <marie@vohralik.net>
2025-02-17 21:14:58 +0100

    psani ukolu

--- project.py
+++ project.py
@@ -1,7 +1,29 @@
-ukoly = ["Přidat nový úkol", "Zobrazit všechny úkoly", "Odstranit úkol", "Konec programu"]
+ukoly = []
 
 def hlavni_menu():
-    for ukol in ukoly:
-        print(f"{ukol}")
+    print("1. Přidat nový úkol \n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
+    input("Vyberte možnost (1-4)")
+    if input == 1:
+        pridat_ukol()
+        input ==2
+        zobrazit_vsechny_ukoly()
+        input ==3
+        odstranit_ukol()
+        input==4
+        print("Konec programu")
+    else:
+        print("Toto je špatná volba, prosím, opakujte výběr")
+        input("Vyberte možnost (1-4)")
 
 hlavni_menu()
+
+def pridat_ukol():
+    nazev =input("Zadejte název úkolu")
+    popis =input("Zadejte popis úkolu")
+    ukoly.append(nazev)
+pridat_ukol()
+
+def zobrazit_vsechny_ukoly():
+    print(ukoly())
+
+def odstranit_ukol():
