2ad030b73c4f87e97e8e7f276823f86993999c61
marie <marie@vohralik.net>
2025-02-18 21:06:09 +0100

    zmeny

--- project.py
+++ project.py
@@ -1,29 +1,41 @@
-ukoly = []
-
 def hlavni_menu():
     print("1. Přidat nový úkol \n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
-    input("Vyberte možnost (1-4)")
+    input("Vyberte možnost (1-4): ")
     if input == 1:
         pridat_ukol()
-        input ==2
+    elif input == 2:
         zobrazit_vsechny_ukoly()
-        input ==3
+    elif input == 3:
         odstranit_ukol()
-        input==4
+    elif input == 4:
         print("Konec programu")
     else:
         print("Toto je špatná volba, prosím, opakujte výběr")
         input("Vyberte možnost (1-4)")
 
-hlavni_menu()
-
 def pridat_ukol():
     nazev =input("Zadejte název úkolu")
     popis =input("Zadejte popis úkolu")
-    ukoly.append(nazev)
-pridat_ukol()
+    ukol={"Název":nazev,"Popis":popis}
+    ukoly.append(ukol)
+
+
 
 def zobrazit_vsechny_ukoly():
-    print(ukoly())
+    print(ukoly)
 
 def odstranit_ukol():
+    zobrazit_vsechny_ukoly()
+    ukol_k_odstraneni=input("Zadejte číslo úkolu k odstranění")
+    ukoly.pop(ukol_k_odstraneni)
+
+
+ukoly = [
+    {"Název": "Ukol jedna", "Popis": "Popis ukolu jedna"},
+    {"Název": "Ukol dva", "Popis": "Popis ukolu dva"},
+    {"Název": "Ukol tri", "Popis": "Popis ukolu tri"},
+]
+
+# hlavni_menu()
+zobrazit_vsechny_ukoly()
+# pridat_ukol()
