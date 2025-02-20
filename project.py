968b43f7f7e0550aefad035209ee964c301f1357
marie <marie@vohralik.net>
2025-02-20 12:47:54 +0100

    zmeny

--- project.py
+++ project.py
@@ -1,13 +1,15 @@
+ukoly =[]
+
 def hlavni_menu():
     print("1. Přidat nový úkol \n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
-    input("Vyberte možnost (1-4): ")
-    if input == 1:
+    volba=int(input("Vyberte možnost (1-4): "))
+    if volba == 1:
         pridat_ukol()
-    elif input == 2:
+    elif volba == 2:
         zobrazit_vsechny_ukoly()
-    elif input == 3:
+    elif volba == 3:
         odstranit_ukol()
-    elif input == 4:
+    elif volba == 4:
         print("Konec programu")
     else:
         print("Toto je špatná volba, prosím, opakujte výběr")
@@ -16,13 +18,18 @@ def hlavni_menu():
 def pridat_ukol():
     nazev =input("Zadejte název úkolu")
     popis =input("Zadejte popis úkolu")
+    if nazev == "" or popis == "":
+        print("Zadali jste prázdný vstup")
+        pridat_ukol()
     ukol={"Název":nazev,"Popis":popis}
     ukoly.append(ukol)
-
-
+    
 
 def zobrazit_vsechny_ukoly():
-    print(ukoly)
+    if not ukoly:
+        print("Žádný úkol není k dispozici")
+    for i in range(len(ukoly)):
+        print(f"{i+1}) {ukoly[i]['Název']} - {ukoly[i]['Popis']}")
 
 def odstranit_ukol():
     zobrazit_vsechny_ukoly()
@@ -30,12 +37,8 @@ def odstranit_ukol():
     ukoly.pop(ukol_k_odstraneni)
 
 
-ukoly = [
-    {"Název": "Ukol jedna", "Popis": "Popis ukolu jedna"},
-    {"Název": "Ukol dva", "Popis": "Popis ukolu dva"},
-    {"Název": "Ukol tri", "Popis": "Popis ukolu tri"},
-]
 
 # hlavni_menu()
+# zobrazit_vsechny_ukoly()
+pridat_ukol()
 zobrazit_vsechny_ukoly()
-# pridat_ukol()
